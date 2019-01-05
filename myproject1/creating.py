c58a84b3d2b39a8fc919604eddcf7da3ad0d455c
garda <gardas.bartek@gmail.com>
2019-01-05 17:37:35 +0100

    p1

--- myproject1/creating.py
+++ myproject1/creating.py
@@ -1,1 +1,24 @@
-from tkinter import *
+def generate_range(start,end):
+    i = start
+    while i <= end:
+        yield i
+        i += 1
+
+for i in generate_range(0,6):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
